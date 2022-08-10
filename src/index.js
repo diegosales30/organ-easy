05a7102dfc1d7d3801c01c4c8d7aedc63e2cabf3
diegosales30 <diegofelipesales23@gmail.com>
2022-08-10 17:10:38 -0400

    criando pasta pages, components, routers e add style global com styled components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <BrowserRouter>
     <React.StrictMode>
