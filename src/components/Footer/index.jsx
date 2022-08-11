f92fc34afab9d317d8269316d3e3330b5b58c89c
diegosales30 <diegofelipesales23@gmail.com>
2022-08-10 23:58:35 -0400

    editando o footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,39 +1,10 @@
 import {
   Box,
   Container,
-  Link,
   Stack,
   Text,
   useColorModeValue,
-  VisuallyHidden,
 } from "@chakra-ui/react";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
-import { SiGmail } from "react-icons/si";
-
-const SocialButton = ({ children, label, href }) => {
-  return (
-    <Link
-      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-      rounded={"full"}
-      w={8}
-      h={8}
-      cursor={"pointer"}
-      as={"a"}
-      href={href}
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{
-        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-      }}
-      target={"_blank"}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </Link>
-  );
-};
 
 const Footer = () => {
   return (
@@ -43,34 +14,13 @@ const Footer = () => {
     >
       <Container
         as={Stack}
-        maxW={"6xl"}
-        py={4}
-        direction={{ base: "column", md: "row" }}
+        maxW={"5xl"}
+        py={5}
         spacing={4}
-        justify={{ base: "center", md: "space-between" }}
-        align={{ base: "center", md: "center" }}
+        justify={"center"}
+        align={"center"}
       >
-        <Text fontSize={"0.8rem"}>© 2022 Desenvolvido por Diego Sales.</Text>
-        <Stack direction={"row"} spacing={6}>
-          <SocialButton
-            label={"GitHub"}
-            href={"https://github.com/diegosales30"}
-          >
-            <FaGithub fontSize={"1.5rem"} />
-          </SocialButton>
-          <SocialButton
-            label={"Linkedin"}
-            href={"https://www.linkedin.com/in/diego-felipe-js/"}
-          >
-            <FaLinkedinIn fontSize={"1.5rem"} />
-          </SocialButton>
-          <SocialButton
-            label={"Gmail"}
-            href={"mailto:diegosalesdasilva23@gmail.com"}
-          >
-            <SiGmail fontSize={"1.5em"} />
-          </SocialButton>
-        </Stack>
+        <Text fontSize={"0.6rem"}>© Desenvolvido por Diego Sales. 2022</Text>
       </Container>
     </Box>
   );
