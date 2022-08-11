2c248f9779b24dda1f2e24bb5b5d3655696538b1
diegosales30 <diegofelipesales23@gmail.com>
2022-08-10 23:49:17 -0400

    criando e editando o header com nightMode

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,18 @@
 import {
   Box,
+  Button,
   Flex,
-  IconButton,
+  Heading,
   Stack,
-  Collapse,
+  Text,
   useColorModeValue,
-  useDisclosure,
-  ListItem,
-  UnorderedList,
 } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
 import NightMode from "../nightMode";
-import { SiHomebridge } from "react-icons/si";
+import { BsBoxArrowRight } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const { isOpen, onToggle } = useDisclosure();
-
   const navigate = useNavigate();
 
   const handleNavigation = (path) => {
@@ -37,70 +33,21 @@ const Header = () => {
         align={"center"}
       >
         <Flex
-          flex={{ base: 1, md: "auto" }}
-          ml={{ base: -2 }}
-          display={{ base: "flex", md: "none" }}
-        >
-          <IconButton
-            onClick={onToggle}
-            icon={
-              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-            }
-            variant={"ghost"}
-            aria-label={"Toggle Navigation"}
-          />
-        </Flex>
-        <Flex
           flex={{ base: 1 }}
-          justify={{ base: "center", md: "start" }}
+          justify={{ base: "start", md: "start" }}
           bg={useColorModeValue("gray.100", "gray.800")}
         >
-          <SiHomebridge
-            size={30}
-            cursor={"pointer"}
-            onClick={() => handleNavigation("/home")}
-          />
-          <Flex>
-            <UnorderedList
-              display={{ base: "none", md: "flex" }}
-              ml={10}
-              alignItems={"center"}
-              listStyleType={"none"}
-            >
-              <ListItem
-                onClick={() => handleNavigation("/home")}
-                cursor={"pointer"}
-                _hover={{ color: "#e3386a" }}
-                marginLeft={"10px"}
-              >
-                Home
-              </ListItem>
-              <ListItem
-                onClick={() => handleNavigation("/tecnologias")}
-                cursor={"pointer"}
-                marginLeft={"20px"}
-                _hover={{ color: "#e3386a" }}
-              >
-                Tecnologias
-              </ListItem>
-              <ListItem
-                onClick={() => handleNavigation("/projetos")}
-                cursor={"pointer"}
-                marginLeft={"20px"}
-                _hover={{ color: "#e3386a" }}
-              >
-                Projetos
-              </ListItem>
-              <ListItem
-                onClick={() => handleNavigation("/contato")}
-                cursor={"pointer"}
-                marginLeft={"20px"}
-                _hover={{ color: "#e3386a" }}
-              >
-                Contato
-              </ListItem>
-            </UnorderedList>
-          </Flex>
+          <Heading
+            bg={useColorModeValue("gray.100", "gray.800")}
+            fontWeight={600}
+            fontSize={{ base: "1xl", sm: "2xl", md: "3xl" }}
+            lineHeight={"110%"}
+          >
+            Organ
+            <Text as={"span"} color={"orange.400"}>
+              Easy
+            </Text>
+          </Heading>
         </Flex>
 
         <Stack
@@ -109,58 +56,18 @@ const Header = () => {
           direction={"row"}
           spacing={6}
         >
+          <Button
+            aria-label="Toggle Color Mode"
+            _focus={{ boxShadow: "none" }}
+            w="fit-content"
+            onClick={() => handleNavigation("/")}
+          >
+            <BsBoxArrowRight />
+          </Button>
+
           <NightMode />
         </Stack>
       </Flex>
-
-      <Collapse in={isOpen} animateOpacity>
-        <Flex>
-          <UnorderedList
-            position={"absolute"}
-            padding={0}
-            margin={0}
-            top={61}
-            //left={-5}
-            bg={useColorModeValue("white", "gray.800")}
-            p={5}
-            display={{ md: "none" }}
-            listStyleType={"none"}
-          >
-            <ListItem
-              onClick={() => handleNavigation("/home")}
-              marginTop={"0px"}
-              cursor={"pointer"}
-              _hover={{ color: "#e3386a" }}
-            >
-              Home
-            </ListItem>
-            <ListItem
-              onClick={() => handleNavigation("/tecnologias")}
-              marginTop={"10px"}
-              cursor={"pointer"}
-              _hover={{ color: "#e3386a" }}
-            >
-              Tecnologias
-            </ListItem>
-            <ListItem
-              onClick={() => handleNavigation("/projetos")}
-              marginTop={"10px"}
-              cursor={"pointer"}
-              _hover={{ color: "#e3386a" }}
-            >
-              Projetos
-            </ListItem>
-            <ListItem
-              onClick={() => handleNavigation("/contato")}
-              marginTop={"10px"}
-              cursor={"pointer"}
-              _hover={{ color: "#e3386a" }}
-            >
-              Contato
-            </ListItem>
-          </UnorderedList>
-        </Flex>
-      </Collapse>
     </Box>
   );
 };
--- src/components/nightMode/index.jsx
+++ src/components/nightMode/index.jsx
@@ -1,13 +1,10 @@
 import { Button, Flex, useColorMode } from "@chakra-ui/react";
-import { BsSun, BsMoonStarsFill } from "react-icons/bs";
+
+import { MdNightsStay, MdOutlineWbSunny } from "react-icons/md";
 
 const NightMode = (ButtonProps) => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    /**
-     * Ideally, only the button component should be used (without Flex).
-     * Props compatible with <Button /> are supported.
-     */
     <Flex justifyContent="center" alignItems="top">
       <Button
         aria-label="Toggle Color Mode"
@@ -15,7 +12,7 @@ const NightMode = (ButtonProps) => {
         _focus={{ boxShadow: "none" }}
         w="fit-content"
       >
-        {colorMode === "light" ? <BsMoonStarsFill /> : <BsSun />}
+        {colorMode === "light" ? <MdNightsStay /> : <MdOutlineWbSunny />}
       </Button>
     </Flex>
   );
