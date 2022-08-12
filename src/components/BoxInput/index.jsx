669fd1abd39b1dbcef73ebf35e7d30f50198d4fd
diegosales30 <diegofelipesales23@gmail.com>
2022-08-12 11:53:02 -0400

    atualizando maxW para 1700

--- src/components/BoxInput/index.jsx
+++ src/components/BoxInput/index.jsx
@@ -10,17 +10,28 @@ const BoxInput = () => {
       w={"100vw"}
       h={"100vh"}
       display={["block", "block", "flex", "flex"]}
-      maxW={"1920px"}
-      minW={"424px"}
+      maxW={"1700px"}
+      minW={"380px"}
       bg={useColorModeValue("white", "gray.800")}
-      justifyContent={"space-around"}
-      padding={"30px"}
+      justifyContent={"space-between"}
+      //padding={"10px"}
+      margin={"auto"}
     >
-      <Flex w={"100%"} justifyContent={"center"} flexDirection={"column"}>
+      <Flex
+        w={"90%"}
+        margin={"auto"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+      >
         <ComponentInput />
         <TotalValue />
       </Flex>
-      <Flex w={"100%"} justifyContent={"center"} flexDirection={"column"}>
+      <Flex
+        w={"90%"}
+        margin={"auto"}
+        justifyContent={"center"}
+        flexDirection={"column"}
+      >
         <ComponentList />
         <RemoveAll />
       </Flex>
